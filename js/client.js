6a2d1704a09e0a0a87e59db753b346330c470ebd
Amit Sureshchandra Kesarwani <akumar00029@gmail.com>
2020-12-29 10:38:47 +0530

    partial working

---
+++ js/client.js
@@ -0,0 +1,30 @@
+const socket = io('http://localhost:8000');
+
+
+
+
+// const io = require("socket.io-client");
+// const socket = io("http://localhost:8000", {
+//   withCredentials: true,
+//   extraHeaders: {
+//     "my-custom-header": "abcd"  
+//   }
+// });
+
+const form = document.getElementById('send-container')
+const messageInput = document.getElementById('messageInp')
+const messageContainer = document.querySelector('.container')
+
+const append = (message,position) => {
+    const messageElement = document.createElement('div')
+    messageElement.innerText = message
+    messageElement.classList.add('message')
+    messageElement.classList.add(position)
+    messageContainer.append(messageElement)
+}
+
+const name = prompt('Enter name to join')
+socket.emit('new-user-joined',name)
+socket.on('user-joined', name => {
+    append(`${name} joined chat`,'right')
+})
---
+++ nodeServer/index.js
@@ -0,0 +1,23 @@
+const io = require('socket.io')(8000,{
+    cors: {
+      origin: "http://127.0.0.1:5500",
+      methods: ["GET", "POST"],
+      allowedHeaders: ["my-custom-header"],
+      credentials: true
+    }
+})
+
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+        // user[socket.id] = name;
+        console.log(name)
+        // emmits everyone except user name
+        socket.broadcast.emit('user-joined');
+    });
+
+    socket.on('send', message => {
+        socket.broadcast.emit('receive',{message : message, name : user[socket.id]});
+    });
+})
