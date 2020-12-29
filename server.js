6f6c89e88b719e525995dd6a52c9ab51e186d1bb
Amit Sureshchandra Kesarwani <akumar00029@gmail.com>
2020-12-29 17:05:28 +0530

    code cleanup

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = new express();
-// const path = require('path');
-
-// app.use(express.static(__dirname + '/public'));
-
-// app.get('/', function(request, response){
-//     response.sendFile(path.join('public/index.html'));
-// });
-
-// var server = app.listen(5000,() => console.log('running at 5000'))
-
 const io = require('socket.io')(5000,{
     cors: {
       origin: "http://localhost:8081",
